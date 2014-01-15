60e3477abad02a9de604d5523849aea3819d7520
OpenShift guest <007@007design.com>
2014-01-15 17:14:14 -0500

    Working on routes

--- formulate/WebContent/scripts/app.js
+++ formulate/WebContent/scripts/app.js
@@ -1,38 +1,62 @@
 /**
  * Create a module
  */
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/', { 
+            resolve: {
+                state: function($route, $location, viewStateSvc){
+                    angular.forEach($route.current.params, function(v, k){
+                        viewStateSvc[k] = v;
+                    });
+                }
+            }
+        })
+        .otherwise({redirectTo: '/'});
+}).run(function($route) {});
+
+app.service('viewStateSvc', function(){
+    var scope = this;
+    return scope;
+});
 
 /**
  * Accordion section directive
  */
-app.directive('accordion', function($compile) {
+app.directive('accordion', function(viewStateSvc, $route) {
     return {
         restrict: 'C',
         compile: function(tElem){
             tElem.children().addClass('accordion-section');
         },
         controller: function(){
-            this.toggle = function(pane){
-                pane.showSection = !pane.showSection;
+            this.showSection = function(pane){
+                console.log(pane);
+                //viewStateSvc.showSection = pane;
+                $route.current.params.showSection = pane;
+                console.log($route.current.params);
             };
         }
     };
 });
 
-app.directive('accordionSection', function(){
+app.directive('accordionSection', function(viewStateSvc){
     return {
         restrict: 'C',
-        scope:true,
+        scope: true,
         require: '^accordion',
         compile: function(tElem, tAttrs, transclude){
-            tElem.find('p').attr('ng-show', 'showSection');
+            tElem.find('p').attr('ng-show', 'viewStateSvc.showSection == route');
             
             return function(scope, elem, attrs, ctrl) {
-                elem.find('h1,h2').bind('click', function(){
-                    console.log(ctrl.showSection);
+                scope.viewStateSvc = viewStateSvc;
+                scope.route = attrs.route;
+                
+                elem.find('h1,h2,h3').bind('click', function(){
                     scope.$apply(function(){
-                        ctrl.toggle(scope);
+                        ctrl.showSection(scope.route);
                     });
                 });
             }
@@ -40,10 +64,35 @@ app.directive('accordionSection', function(){
    }; 
 });
 
-(function($){
-	$(document).ready(function(){
-			
-		/* Bootstrap the app */
-		angular.bootstrap($('body'), ['app']);
-	});
-})(jQuery);
+app.directive('tabs', function(viewStateSvc){
+    return {
+        restrict: 'C',
+        controller: function(){
+            this.showTab = function(tab){
+                viewStateSvc.showTab = tab;
+            };
+        }
+    };
+});
+
+app.directive('tab', function(viewStateSvc){
+    return {
+        restrict: 'C',
+        require: '^tabs',
+        compile: function(tElem, tAttrs, transclude){
+            tElem.attr('ng-show', 'viewStateSvc.showTab == tab')
+            
+            return function(scope, elem, attrs, ctrl){
+                scope.viewStateSvc = viewStateSvc;
+                scope.tab = attrs.tab;
+            }
+        }
+    };
+});
+
+app.controller('viewStateCtrl', function($scope, $route){
+    $scope.$on('$routeChangeSuccess', function(){
+        console.log('viewStateSvc init');
+        console.log($route.current.params);
+    });
+});
